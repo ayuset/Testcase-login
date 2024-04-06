6e9d53f7817520522a29057dedfe68e959bc441d
ayuset <ayusetyaningtyas16@gmail.com>
2024-04-07 00:33:49 +0700

    Add files via upload

---
+++ Negative Testcase_Login3.py
@@ -0,0 +1,26 @@
+from selenium import webdriver
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+
+from selenium.webdriver.chrome.options import Options
+
+
+chrome_options = Options()
+chrome_options.add_experimental_option("detach", True)
+
+driver = webdriver.Chrome(options=chrome_options)
+driver.get("https://www.psegameshop.com/my-account/")
+driver.maximize_window()
+username = driver.find_element(By.ID, "username")
+username.clear()
+username.send_keys("test")
+password = driver.find_element(By.ID, "password")
+password.clear()
+password.send_keys("123")
+login = driver.find_element(By.NAME, "login")
+login.click()
+
+driver.close()
+
+
